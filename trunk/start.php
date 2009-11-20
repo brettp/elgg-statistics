2485eab57465503f9355118bf98c586ba2cfcb1d
brett.profitt <brett.profitt@04dfd658-d613-11de-9196-2db6e9c7c7ae>
2009-11-20 21:22:45 +0000

    Added flot, manifest.xml.

--- trunk/start.php
+++ trunk/start.php
@@ -1,28 +1,51 @@
 <?php
+/**
+ *
+ */
 
+/**
+ * Start plugin
+ *
+ * @return null
+ */
 function elgg_statistics_init(){
-
-    register_page_handler("statistics","elgg_statistics_pagehandler");
+	register_page_handler("statistics","elgg_statistics_pagehandler");
 }
 
+/**
+ * Add tools menu for admins
+ *
+ * @return null
+ */
 function elgg_statistics_pagesetup(){
-    global $CONFIG;
-    if(get_context()== "admin" && isadminloggedin()){
-        add_submenu_item(elgg_echo("elgg_statistics:statistics"),"pg/statistics/");
-    }
+	global $CONFIG;
+	if(get_context()== "admin" && isadminloggedin()){
+		add_submenu_item(elgg_echo("elgg_statistics:statistics"),"pg/statistics/");
+	}
 
 }
 
+/**
+ * Handle pages for stats
+ *
+ * @param $page
+ * @return str
+ */
 function elgg_statistics_pagehandler($page){
 
-    switch($page[0]){
-        default:
-            elgg_statistics_admin_page();
-    }
+	switch($page[0]){
+		default:
+			elgg_statistics_admin_page();
+	}
 }
 
+/**
+ * Serve up the admin page.
+ * @return str
+ */
 function elgg_statistics_admin_page(){
+
 }
 
-register_elgg_event_handler('init','system','elgg_statistics_init');
-register_elgg_event_handler('pagesetup','system','elgg_statistics_pagesetup');
+register_elgg_event_handler('init', 'system', 'elgg_statistics_init');
+register_elgg_event_handler('pagesetup', 'system', 'elgg_statistics_pagesetup');
