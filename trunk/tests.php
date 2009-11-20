cb7c8af01e1d43c7389e5ffa6c635c2e4a1e22c4
brett.profitt <brett.profitt@04dfd658-d613-11de-9196-2db6e9c7c7ae>
2009-11-20 22:00:51 +0000

    Added unit tests as an example.

---
+++ trunk/tests.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Elgg Test ElggEntities
+ *
+ * @package Elgg
+ * @subpackage Test
+ * @author Curverider Ltd
+ * @link http://elgg.org/
+ */
+class ElggPluginStatisticsTest extends ElggCoreUnitTest {
+	/**
+	 * Called before each test method.
+	 */
+	public function setUp() {
+		//$this->entity = new ElggEntityTest();
+	}
+
+	/**
+	 * Called after each test method.
+	 */
+	public function tearDown() {
+//		$this->swallowErrors();
+//		unset($this->entity);
+	}
+
+	public function testStatistics() {
+		$this->assertTrue(TRUE);
+	}
+}
